708e326daa8656775985e4c221304f2888509003
Ranjit Thapa <thaparanjit4567@gmail.com>
2025-12-09 21:05:49 +0545

    Second

---
+++ greatestNumber.py
@@ -0,0 +1,11 @@
+
+
+a=input("Enter first number")
+b=input("Enter second number")
+c=input("Enter third number")
+if(a>b>c):
+        print(a,"is greatest.")
+elif(a<b>c):
+        print(b,"is greatest.")     
+else:   
+     print(c,"is greatest.")
