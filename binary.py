ae923a722c4e28c85b1c2fd51c30a5d60ffea061
Ranjit Thapa <thaparanjit4567@gmail.com>
2025-12-16 21:15:25 +0545

    Number System

---
+++ binary.py
@@ -0,0 +1,5 @@
+# convert binary to decimal, octal and hexadecimal
+num = int(input("Enter a binary number: "))
+print("Decimal: ", int(num, 2))
+print("Octal: ", oct(num))
+print("Hexadecimal: ", hex(num))
---
+++ decimal.py
@@ -0,0 +1,8 @@
+# convert decimal to binary, octal and hexadecimal
+num = int(input("Enter a decimal number: "))
+print("Binary: ", bin(num))
+print("Octal: ", oct(num))
+print("Hexadecimal: ", hex(num))
+
+
+
