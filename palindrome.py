27980c9e0668775a905d06ce452654ca34da9bb5
Ranjit Thapa <thaparanjit4567@gmail.com>
2025-12-16 21:17:55 +0545

    Palindrome

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+#check the palinndrome
+num = int(input("Enter a number: "))
+temp = num
+rev = 0
+while temp > 0:
+    digit = temp % 10
+    rev = rev * 10 + digit
+    temp = temp // 10
+if num == rev:
+    print(num, "is a palindrome")
+else:
+    print(num, "is not a palindrome")
