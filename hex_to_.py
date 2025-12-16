189620c7badd6abd3147e590b97a6900ca99b036
Ranjit Thapa <thaparanjit4567@gmail.com>
2025-12-16 21:13:56 +0545

    Number System

---
+++ hex_to_.py
@@ -0,0 +1,5 @@
+# convert hexadecimal to decimal, binary and octal
+num = int(input("Enter a hexadecimal number: "))
+print("Decimal: ", int(num, 16))
+print("Binary: ", bin(num))
+print("Octal: ", oct(num))
